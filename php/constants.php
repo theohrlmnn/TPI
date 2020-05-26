1f64081af3cf7e92266c652845502624a1fdcaed
tewex <theo.hrlmn@eduge.ch>
2020-05-26 10:16:24 +0200

    ajout const roles

--- php/constants.php
+++ php/constants.php
@@ -4,21 +4,11 @@
  */
 
  // Les rôles définis dans la base de données
- // @see ROLES table
-define ('ERL_STUDENT', 1);
-define ('ERL_ADMIN', 99);
+ // @see roles table
+define ('RL_Nobody', 5);
+define ('RL_Candidate', 4);
+define ('RL_Manager', 3);
+define ('RL_Expert', 2);
+define ('RL_Administrator', 1);
 
-/**
- * @brief Les constantes pour les contextes de la search bar
- */
-define ('SB_CONTEXT_NONE', 0);
-define ('SB_CONTEXT_ADS', 1);
-define ('SB_CONTEXT_USER', 2);
-define ('SB_CONTEXT_GESTION', 3);
-
- // Les statuts des annonces définis dans la base de données
- // @see STATUS table
-define ('ADS_STATUS_ACTIVATED', 1);
-define ('ADS_STATUS_DEACTIVATED', 0);
-define ('ADS_STATUS_ALL', -1);
 ?>
