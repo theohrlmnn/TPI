82284d2f11cc42f262fc1c413c70d9a84c6fa8b0
tewex <theo.hrlmn@eduge.ch>
2020-06-04 15:32:50 +0200

    changer couleur navigation + modification

--- php/includes/nav.php
+++ php/includes/nav.php
@@ -1,50 +1,120 @@
 <div class="uk-offcanvas-content">
   <!-- menu position. delete .uk-light to change black navbar to white (also you should change logo to dark one)-->
-  <nav class="uk-navbar-container uk-light" uk-navbar="mode: click" uk-sticky="animation: uk-animation-slide-top; show-on-up: true">
-    <!-- menu-->
+  <nav class="uk-navbar-container color" uk-navbar="mode: click">
     <div class="uk-navbar-left nav-overlay">
       <div class="uk-navbar-flip">
-        <ul class="uk-navbar-nav uk-visible@s">
-          <li><a href="index.php"><i class="fas fa-home"></i>&nbsp;HOME</a></li>
-          <li><a href="post.php"><i class="far fa-plus-square"></i>&nbsp;POST </a></li>
-        </ul>
-        <ul class="uk-navbar-nav uk-hidden@s">
+        <ul class="uk-navbar-nav">
+          <li><a href="#">
+              <img src="./img/LogoExpertsDev.svg" width="30" height="30" class="" alt="">
+              &nbsp;CdEIG</a></li>
           <li><a class="uk-navbar-toggle" uk-navbar-toggle-icon uk-toggle="target: #mobile-navbar"></a></li>
+
+        </ul>
+      </div>
+    </div>
+    <!-- menu-->
+    <div class="uk-navbar-right nav-overlay">
+      <div class="uk-navbar-flip">
+        <ul class="uk-navbar-nav">
+          <div class="uk-margin-right uk-navbar-nav uk-navbar-right"><a class="uk-navbar-item uk-logo" href="#">
+              <li class="uk-navbar-right ">
+                <a href="home.php">A propos&nbsp;</a>
+              </li>
+              <li class="uk-navbar-right ">
+                <a href="#">Administration&nbsp;</a>
+              </li>
+              <li class="uk-navbar-right ">
+                <a href="redactionTPI.php">Rédaction&nbsp;</a>
+              </li>
+              <li class="uk-navbar-right ">
+                <a href="#">Répartition&nbsp;</a>
+              </li>
+              <li class="uk-navbar-right ">
+                <a href="#">Validation&nbsp;</a>
+              </li>
+              <li class="uk-navbar-right ">
+                <a href="#">Evaluation&nbsp;</a>
+              </li>
+              <li>
+                <span uk-icon="icon: user; ratio: 2"></span>
+                <div class="uk-navbar-dropdown">
+                  <ul class="uk-nav uk-navbar-dropdown-nav">
+                    <li><a href="#">Paramètre</a></li>
+                    <li><a href="logout.php">Deconnexion</a></li>
+                  </ul>
+                </div>
+                <span uk-icon="icon: chevron-down"></span>
+              </li>
+            </a>
+          </div>
+
         </ul>
       </div>
     </div>
     <!-- endmenu-->
     <!-- logo or title-->
-    <div class="uk-navbar-right nav-overlay"><a class="uk-navbar-item uk-logo" href="#">
-        <!--h3 Site Name--><img src="img/profile/feuille.png" alt="logo" width="64"></a></div>
+
+
     <!-- end logo or title-->
 
   </nav>
   <!-- end menu position-->
-  <!-- off-canvas menu-->
-  <div id="mobile-navbar" uk-offcanvas="mode: slide; flip: false">
-    <div class="uk-offcanvas-bar">
-      <!-- off-canvas close button-->
 
-      <!-- off-canvas close button-->
-      <ul class="uk-nav-default uk-nav-parent-icon" uk-nav>
-        <!-- logo or title-->
-        <li class="uk-text-center" style="padding: 0 0 25px 0;"><a href="#">
-            <!--h3 Site Name--><img src="img/profile/feuille.png" alt="logo" width="64"></a></li>
-        <!-- end logo or title-->
-        <!-- menu-->
-        <li>
-          <hr>
-        </li>
-        <li class="uk-text-center">
-          <h3>Menu</h3>
-        </li>
-        <li><a href="index.php"><i class="fas fa-home"></i>&nbsp;HOME
-            <!--span.uk-light(uk-icon="icon: pencil")-->
-            <!--| #{" "}Item 1--></a></li>
-        <li><a href="post.php"><i class="far fa-plus-square"></i>&nbsp;POST
-            <!--span.uk-light(uk-icon="icon: code")-->
-            <!--| #{" "}Item #{i}#{j}--></a></li>
-      </ul>
+</div>
+<!-- off-canvas menu-->
+<div id="mobile-navbar" uk-offcanvas="mode: push; flip: false">
+  <div class="uk-offcanvas-bar">
+    <!-- off-canvas close button-->
+
+    <!-- off-canvas close button-->
+    <ul uk-nav>
+      <!-- menu-->
+      <li class="uk-text-center">
+        <h4><a href="">Administration</a></h4>
+      </li>
+
+      <li><a href="index.php"> Action non implémentée</a></li>
+      <li class="uk-text-center">
+        <h4 class="uk-margin-top"><a href="redactionToi.php">Rédaction</a></h4>
+      </li>
+      <?php
+      $arrRight = getRightUserSession();
+      foreach ($arrRight as $r) {
+        switch ($r) {
+          case 'createTPI':
+            echo "<li><a href=\"createTPI.php\">Créer un TPI</a></li>";
+            break;
+          case 'listTPI':
+            echo "<li><a href=\"listTPI.php\">Liste TPI</a></li>";
+            break;
+          case 'viewPDF':
+            echo "<li><a href=\"viewPDF.php\">Voir l'énoncé</a></li>";
+            break;
+        }
+      } ?>
+      <li class="uk-text-center">
+        <h4 class="uk-margin-top">Répartition</h4>
+      </li>
+      <li><a href="index.php">Action non implémentée</a></li>
+      <li class="uk-text-center">
+        <h4 class="uk-margin-top">Validation</h4>
+      </li>
+      <li><a href="index.php">Action non implémentée</a></li>
+      <li class="uk-text-center">
+        <h4 class="uk-margin-top">Evaluation</h4>
+      </li>
+      <li><a href="index.php">Action non implémentée</a></li>
+    </ul>
+    <div class="uk-margin uk-margin-medium-top">
+      <p class="uk-margin-remove">Connecté en tant que :</p>
+      <p class="uk-margin-remove">
+        <?= getLastNameUserSession() ?>
+        <?= getFirstNameUserSession() ?>
+      </p>
+      <p class="uk-margin-remove">
+        <?= getRoleNameUserSession() ?>
+      </p>
     </div>
   </div>
+
+</div>
